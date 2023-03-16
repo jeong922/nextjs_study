7d527a026667d72e158a7165826bbad219d40ffa
jeong922 <hwajeong922@naver.com>
2023-03-16 16:55:04 +0900

    동적 라우팅

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({ params }: Props) {
+  return <h1>{params.slug} 제품 설명 페이지</h1>;
+}

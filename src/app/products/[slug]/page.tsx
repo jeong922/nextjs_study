94e5907eb545add8c36428fe1994482411224236
jeong922 <hwajeong922@naver.com>
2023-03-16 17:10:46 +0900

    generateStaticParams 사용

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+//https://nextjs.org/docs/basic-features/pages
+// 13버전
+// https://beta.nextjs.org/docs/routing/fundamentals
+// https://beta.nextjs.org/docs/routing/defining-routes
+
 type Props = {
   params: {
     slug: string;
@@ -8,4 +13,13 @@ type Props = {
 
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+// ❓ 특정 경로에 대해 미리 페이지를 만드는 경우
+// generateStaticParams라는 이름은 규격 사항
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
 }

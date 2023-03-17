cb69509d4ac64f4dbb78bdf68bc640755cc372e3
jeong922 <hwajeong922@naver.com>
2023-03-17 20:37:56 +0900

    not found

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 //https://nextjs.org/docs/basic-features/pages
@@ -12,10 +13,14 @@ type Props = {
 };
 
 export default function PantsPage({ params }: Props) {
+  if (params.slug === 'nothing') {
+    notFound();
+  }
   return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
 
 // ❓ 특정 경로에 대해 미리 페이지를 만드는 경우
+// https://beta.nextjs.org/docs/data-fetching/generating-static-params
 // generateStaticParams라는 이름은 규격 사항
 export function generateStaticParams() {
   const products = ['pants', 'skirt'];

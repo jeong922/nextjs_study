7630b489754cb58a859254ea136affe7b03e5154
jeong922 <hwajeong922@naver.com>
2023-03-20 20:57:00 +0900

    서버 컴포넌트, 클라이언트 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
-import { notFound } from 'next/navigation';
+import os from 'os'; // 노드 API
+import { useState } from 'react';
+import Counter from '@/components/Counter';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  // notFound();
-  return <h1>홈페이지</h1>;
+  console.log('안녕! - 서버');
+  console.log(os.hostname());
+  return (
+    <>
+      <h1>홈페이지</h1>
+      <Counter />
+    </>
+  );
 }

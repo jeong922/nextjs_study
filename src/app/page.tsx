1c3fbd1586178cb316b202f46d2c5b91595a9be1
jeong922 <hwajeong922@naver.com>
2023-03-22 11:18:20 +0900

    폰트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from './page.module.css';
 import os from 'os'; // 노드 API
-import { useState } from 'react';
 import Counter from '@/components/Counter';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   console.log('안녕! - 서버');
